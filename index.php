8821794a8997cd779342fb706670dd07e2533137
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-10 22:43:19 +0800

    commit

--- index.php
+++ index.php
@@ -333,7 +333,8 @@
 			                        <a href="admin/admin_login.php">Admin Login</a>
                                     <a href="admin/index.php">Organizer Login</a>
                                     <a href="tabulator/index.php">Tabulator Login</a>
-                                    <a href="student/index.php">Student Login</a>
+                                    <a href="admin/welcome.php">Judge Login</a>
+
                                 </div>
                             </li>
                         </ul>
--- ongoing.php
+++ ongoing.php
@@ -297,8 +297,7 @@
                     <a href="admin/index.php">Organizer Login</a>
                     <a href="tabulator/index.php">Tabulator Login</a>
                     <a href="judge/index.php">Judge Login</a>
-                    <a href="student/index.php">Student Login</a>
-                    </div>
+                </div>
             </li>
         </ul>
     </div>
