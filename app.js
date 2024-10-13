63ef20d39fbfccf7082992ccc294f350e6150129
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-13 18:48:32 +0800

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('https').createServer(app);
-const io = require('socket.io')(http, {
+const io = require('socket.io')(https, {
     cors: {
         origin: "*",
         methods: ["GET", "POST"]
