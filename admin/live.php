9d3e784ed0546c224a982035650d14de1b2cab74
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-13 10:15:18 +0800

    commit

--- admin/live.php
+++ admin/live.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 // Database connection (replace with your actual database credentials)
-$db = new mysqli('localhost', 'root', '', 'judging');
+$db = new mysqli('127.0.0.1', 'u510162695_judging_root', '1Judging_root', 'u510162695_judging');
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
 }
