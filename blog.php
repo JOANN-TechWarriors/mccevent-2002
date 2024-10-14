6cf672cf20c26d2571de970b19fb5c484e66f087
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-14 20:05:36 +0800

    commit

--- blog.php
+++ blog.php
@@ -25,7 +25,7 @@ $result = $db->query("SELECT * FROM streams WHERE status = 'live'");
             border-radius: 5px;
         }
         .stream-video {
-            width: 1320px;
+            width: 1300px;
             height: 580px;
             background-color: #000;
             color: #fff;
