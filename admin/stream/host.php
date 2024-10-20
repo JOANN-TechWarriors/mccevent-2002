6b01891e28acec3ae6b6b509080d1256fc735c55
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-20 21:11:58 +0800

    commit

--- admin/stream/host.php
+++ admin/stream/host.php
@@ -58,7 +58,7 @@ $stmt->close();
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-    <link rel="shortcut icon" href="../img/logo.png"/>
+    <link rel="shortcut icon" href="../images/logo copy.png" />
     <link rel="stylesheet" href="assets/css/m2m-live.css">
     <link rel="icon" href="assets/img/favicon.png" type="image/png">
     <link rel="apple-touch-icon" href="assets/img/apple-touch-icon.png" type="image/png">
