bbeffd8e636ed90307a78741791db8926be60e66
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-20 21:10:14 +0800

    commit

--- admin/live_stream.php
+++ admin/live_stream.php
@@ -149,7 +149,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="en">
 
 <head>
-    <link rel="shortcut icon" href="../img/logo.png" />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
