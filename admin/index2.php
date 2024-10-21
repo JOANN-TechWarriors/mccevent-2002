ee5e079b5651476c08191561332c0db5b513199f
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-21 18:40:22 +0800

    commit

--- admin/index2.php
+++ admin/index2.php
@@ -87,7 +87,7 @@ thead th {
              <br/> 
              <table cellpadding="50" cellspacing="40"  align="center">
              <thead>
-             <th align="left" style="background-color: aquamarine; text-indent: 10px; color: black; "><h4>ORGANIZER  </h4></th>
+             <th align="left" style="background-color: aquamarine; text-indent: 10px; color: black; "><h4>ORGANIZER LOGIN </h4></th>
              </thead>
              
              <tr style="background-color: #fff;">
