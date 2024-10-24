69bc29e71bd4b12fb372ec0f4729b51a20f1c1a6
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-24 17:30:32 +0800

    commit

--- admin/doc_header.php
+++ admin/doc_header.php
@@ -7,9 +7,9 @@
      <tr>
       <td>
       <center >
-      <font size="5"><strong><?php echo $company_name; ?></strong></font><br />
-        <font size="4"><?php echo $company_address; ?></font> <br />
-        <font size="4"><?php echo $company_telephone; ?> &middot;
+      <font size="4"><strong><?php echo $company_name; ?></strong></font><br />
+        <font size="3"><?php echo $company_address; ?></font> <br />
+        <font size="3"><?php echo $company_telephone; ?> &middot;
          <a href="<?php echo $company_website; ?>" target="_blank"><?php echo $company_website; ?></a> &middot; 
         
         <?php echo $company_email; ?><br />
