f6d5454d205bf0cf7712a5508ff59a957832cf50
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-25 20:11:10 +0800

    commit

--- admin/sub_event.php
+++ admin/sub_event.php
@@ -425,7 +425,7 @@ body {
     /* Table Container */
 .table-responsive-container {
     width: 100%;
-    max-width: 1100px;
+    max-width: 100px;
     margin: 20px 0;
     background: #fff;
     padding: 20px;
