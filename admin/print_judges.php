f479eded7dadc35dcb474708543ac16b4f20032b
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-25 19:10:22 +0800

    commit

--- admin/print_judges.php
+++ admin/print_judges.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Judge List</title>
     <!-- Bootstrap 5 CSS -->
+    <link rel="shortcut icon" href="../images/logo copy.png"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         /* Paper-like background styling */
--- admin/rev_main_event.php
+++ admin/rev_main_event.php
@@ -6,6 +6,7 @@
 <html lang="en">
 <head>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+  <link rel="shortcut icon" href="../images/logo copy.png"/>
   <style>
     body {
       font-family: Arial, sans-serif;
