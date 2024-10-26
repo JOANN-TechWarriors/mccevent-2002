fe813dae9e0d5dc84d4f34b4f960900e86ab9436
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-26 13:24:00 +0800

    commit

--- admin/edit_judge.php
+++ admin/edit_judge.php
@@ -25,6 +25,8 @@
           padding: 0;
           min-height: 100vh; /* Ensures the body takes at least the full viewport height */
           width: 100vw;      /* Ensures full width */
+          overflow-y: auto;  /* Enables vertical scrolling */
+          overflow-x: hidden; /* Prevents horizontal scrolling if content overflows */
           
         }
     .sidebar {
