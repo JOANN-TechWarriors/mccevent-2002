56415f4216297c0fe26ba1cd33dc20287363bcb5
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-27 17:38:19 +0800

    commit

--- about.php
+++ about.php
@@ -28,20 +28,17 @@
       <link rel="stylesheet" href="css1/jquery.mCustomScrollbar.min.css">
       <!-- Tweaks for older IEs-->
       <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
-   </head>
-   <style type="text/css">
-        *{
+
+      <style>
+        * {
             margin: 0;
             padding: 0;
             box-sizing: border-box;
             transition: all 0.4s;
-            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
         }
-        .container{
-            margin-left: 5%;
-            margin-right: 5%;
-        }
-        .nav{
+
+        .nav {
             width: 100%;
             height: 65px;
             position: fixed;
@@ -49,180 +46,58 @@
             text-align: center;
             background-color: rgba(6, 6, 7, 0.8);
             z-index: 1000;
+            top: 0;
+            left: 0;
+            display: flex;
+            justify-content: space-between;
         }
-        .nav div.logo{
-            width: 180px;
-            height: 10px;
-            position: absolute;
+
+        .nav .logo {
+            float: left;
+            width: auto;
+            height: 65px;
+            padding-left: 1rem;
         }
-        .nav div.logo a{
+
+        .nav .logo a {
             text-decoration: none;
             color: #fff;
             font-size: 25px;
             text-transform: uppercase;
         }
-        .nav div.logo a:hover {
-            color: #c0c0c0;
+
+        .nav .logo img {
+            height: 60px;
+            vertical-align: middle;
         }
-        .nav div.main_list{
-            width: 600px;
+
+        .nav .main_list {
             height: 65px;
             float: right;
+            margin-right: 1rem;
         }
-        .nav div.main_list ul{
-            width:100%;
+
+        .main_list ul {
+            width: 100%;
             height: 65px;
             display: flex;
             list-style: none;
-        }
-        .nav div.main_list ul li{
-            width: 120px;
-            height: 65px;
-        }
-        .nav div.main_list ul li a{
-            text-decoration: none;
-            color: #fff;
-            line-height: 65px;
-            text-transform: uppercase;
-        }
-        .nav div.main_list ul li a:hover{
-            color: #c0c0c0;
-        }
-        .nav div.media_button {
-            width: 40px;
-            height: 40px;
-            background-color: transparent;
-            position: absolute;
-            right: 15px;
-            top: 12px;
-            display: none;
-        }
-        .nav div.media_button button.main_media_button {
-            width: 100%;
-            height: 100%;
-            background-color: transparent;;
-            outline: 0;
-            border: none;
-            cursor: pointer;
-        }
-        .nav div.media_button button.main_media_button span{
-            width: 98%;
-            height: 1px;
-            display: block;
-            background-color: #fff;
-            margin-top: 9px;
-            margin-bottom: 10px;
-        }
-        .nav div.media_button button.main_media_button:hover span:nth-of-type(1){
-            transform: rotateY(180deg);
-            transition: all 0.5s;
-            background-color: #c0c0c0;
-        }
-        .nav div.media_button button.main_media_button:hover span:nth-of-type(2){
-            transform: rotateY(180deg);
-            transition: all 0.4s;
-            background-color: #c0c0c0;
-        }
-        .nav div.media_button button.main_media_button:hover span:nth-of-type(3){
-            transform: rotateY(180deg);
-            transition: all 0.3s;
-            background-color: #c0c0c0;
-        }
-        .nav div.media_button button.active span:nth-of-type(1) {
-            transform: rotate3d(0, 0, 1, 45deg);
-            position: absolute;
             margin: 0;
-        }
-        .nav div.media_button button.active span:nth-of-type(2) {
-            display: none;
-        }
-        .nav div.media_button button.active span:nth-of-type(3) {
-            transform: rotate3d(0, 0, 1, -45deg);
-            position: absolute;
-            margin: 0;
-        }
-        .nav div.media_button button.active:hover span:nth-of-type(1) {
-            transform: rotate3d(0, 0, 1, 20deg);
-        }
-        .nav div.media_button button.active:hover span:nth-of-type(3) {
-            transform: rotate3d(0, 0, 1, -20deg);
-        }
-        .fa {
-            padding: 10px;
-            font-size: 10px;
-            width: 8px;
-            text-align: center;
-            text-decoration: none;
-            margin: 5px 5px;
-            border-radius: 30%;
-        }
-        .fa:hover {
-            opacity: 0.5;
-        }
-        .home{
-            background-size: cover;
-            background-position: center;
-            height: 100vh;
-        }
-        @media screen and (min-width: 768px) and (max-width: 1024px) {
-            .container{
-                margin: 0;
-            }
-        }
-        @media screen and (max-width:768px) {
-            .container{
-                margin: 0;
-            }
-            .nav div.logo{
-                margin-left: 15px;
-            }
-            .nav div.main_list{
-                width: 100%;
-                margin-top: 65px;
-                height: 0px;
-                overflow: hidden;
-            }
-            .nav div.show_list{
-                height: 200px;
-            }
-            .nav div.main_list ul{
-                flex-direction: column;
-                width: 100%;
-                height: 200px;
-                top: 80px;
-                right: 0;
-                left: 0;
-            }
-            .nav div.main_list ul li{
-                width: 100%;
-                height: 40px;
-                background-color:rgba(6, 6, 7, 0.8);
-            }
-            .nav div.main_list ul li a{
-                text-align: center;
-                line-height: 40px;
-                width: 100%;
-                height: 40px;
-                display: table;
-            }
-            .nav div.media_button{
-                display: block;
-            }
-        }
-        .main_list ul {
-            list-style-type: none;
             padding: 0;
         }
 
         .main_list ul li {
-            display: inline-block;
             position: relative;
+            display: inline-block;
+            height: 65px;
+            padding: 0 1rem;
         }
 
         .main_list ul li a {
             text-decoration: none;
-            padding: 10px;
-            color: #000;
+            color: #fff;
+            line-height: 65px;
+            text-transform: uppercase;
         }
 
         .main_list ul li:hover .dropdown {
@@ -232,62 +107,151 @@
         .dropdown {
             display: none;
             position: absolute;
-            background-color: black;
-            min-width: 160px;
+            background-color: rgba(6, 6, 7, 0.9);
+            min-width: 200px;
             box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
             z-index: 1;
+            right: 0;
         }
 
         .dropdown a {
-            color: black;
+            color: #fff;
             padding: 12px 16px;
             text-decoration: none;
             display: block;
+            line-height: 1.5;
+            text-align: left;
         }
 
         .dropdown a:hover {
-            background-color: #333;
-            color:red;
+            background-color: rgba(255, 255, 255, 0.1);
         }
-    </style>
-   <body>
-   <nav class="nav">
-    <div class="container">
-        <div class="logo">
-            <a href="#" style="font-family: impact; color: #1153D0;">
-                <img src="img/logo.png" style="height: 40px;  vertical-align: middle;"> MCC Event
-            </a>
-             <span class="text-light"MCC>
-        </div>
-        <div class="main_list" id="mainListDiv">
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="ongoing.php">Ongoing</a></li>
-            <li><a href="upcoming.php">Upcoming</a></li>
-            <li><a href="about.php">About</a></li>
-            <li><a href="admin/stream/index.php">Live</a></li>
-            <li>
-                <a href="#login">Login</a>
-                <div class="dropdown">
-                    <a href="admin/admin_login.php">Admin Login</a>
-                    <a href="admin/index2.php">Organizer Login</a>
-                    <a href="tabulator/index.php">Tabulator Login</a>
-                    <a href="judge/index.php">Judge Login</a>
-                    <a href="student/index.php">Student Login</a>
-                    </div>
-            </li>
-        </ul>
-    </div>
-        <div class="media_button">
-            <button class="main_media_button" id="mediaButton">
-                <span></span>
-                <span></span>
-                <span></span>
-            </button>
-        </div>
-    </div>
-</nav>
-      <!-- header section end -->
+
+        .media_button {
+            display: none;
+            position: absolute;
+            right: 1rem;
+            top: 12px;
+            padding: 0.5rem;
+            cursor: pointer;
+            background: transparent;
+            border: none;
+        }
+
+        .media_button span {
+            width: 32px;
+            height: 3px;
+            background-color: #fff;
+            display: block;
+            margin: 6px 0;
+            transition: 0.3s;
+        }
+
+        .about_section {
+            padding-top: 100px; /* Added to account for fixed navbar */
+        }
+
+        .fa {
+            padding: 10px;
+            font-size: 10px;
+            width: 8px;
+            text-align: center;
+            text-decoration: none;
+            margin: 5px 5px;
+            border-radius: 30%;
+        }
+
+        .fa:hover {
+            opacity: 0.5;
+        }
+
+        @media screen and (max-width: 768px) {
+            .media_button {
+                display: block;
+            }
+
+            .nav .main_list {
+                width: 100%;
+                height: 0;
+                overflow: hidden;
+                position: absolute;
+                top: 65px;
+                left: 0;
+                margin: 0;
+                background-color: rgba(6, 6, 7, 0.95);
+                transition: height 0.3s ease-in-out;
+            }
+
+            .nav .main_list.show_list {
+                height: auto;
+            }
+
+            .main_list ul {
+                flex-direction: column;
+                height: auto;
+                padding: 1rem 0;
+            }
+
+            .main_list ul li {
+                width: 100%;
+                height: auto;
+                padding: 0;
+            }
+
+            .main_list ul li a {
+                line-height: 45px;
+                display: block;
+                padding: 0 2rem;
+                text-align: left;
+            }
+
+            .dropdown {
+                position: static;
+                width: 100%;
+                background-color: rgba(255, 255, 255, 0.1);
+                box-shadow: none;
+            }
+
+            .dropdown a {
+                padding-left: 3rem;
+            }
+        }
+      </style>
+   </head>
+   <body style="background-color: lightgray;">
+      <!-- Updated Navigation Bar -->
+      <nav class="nav">
+          <div class="logo">
+              <a href="#" style="font-family: impact; color: #1153D0;">
+                  <img src="img/logo.png" style="height: 60px; vertical-align: middle;"> MCC Event
+              </a>
+          </div>
+          <div class="main_list" id="mainListDiv">
+              <ul>
+                  <li><a href="index.php">Home</a></li>
+                  <li><a href="ongoing.php">Ongoing</a></li>
+                  <li><a href="upcoming.php">Upcoming</a></li>
+                  <li><a href="about.php">About</a></li>
+                  <li><a href="admin/stream/index.php">Live</a></li>
+                  <li>
+                      <a href="#login">Login</a>
+                      <div class="dropdown">
+                          <a href="admin/admin_login.php">Admin Login</a>
+                          <a href="admin/index2.php">Organizer Login</a>
+                          <a href="tabulator/index.php">Tabulator Login</a>
+                          <a href="admin/welcome.php">Judge Login</a>
+                          <a href="student/index.php">Student Login</a>
+                      </div>
+                  </li>
+              </ul>
+          </div>
+          <button class="media_button" id="mediaButton">
+              <span></span>
+              <span></span>
+              <span></span>
+          </button>
+      </nav>
+
       <!-- about section start -->
       <div class="about_section layout_padding">
          <div class="container">
@@ -296,7 +260,7 @@
                   <div class="col-md-6"> 
                      <div class="about_taital_box">
                         <h1 class="about_taital">ABOUT</h1>
-                        <p class=" about_text">Madridejos Community College (MCC) is an educational institution that serves the local community in Madridejos, a municipality in the province of Cebu, Philippines. It is dedicated to providing quality education and fostering a supportive learning environment for its students.</p>
+                        <p class="about_text">Madridejos Community College (MCC) is an educational institution that serves the local community in Madridejos, a municipality in the province of Cebu, Philippines. It is dedicated to providing quality education and fostering a supportive learning environment for its students.</p>
                         <div class="readmore_btn"><a href="#">Read More</a></div>
                      </div>
                   </div>
@@ -309,17 +273,19 @@
          </div>
       </div>
       <!-- about section end -->
+
       <!-- copyright section start -->
       <div class="copyright_section">
          <div class="container">
             <div class="row">
                <div class="col-sm-12">
-                  <p class="copyright_text"><strong> Event Judging  System &COPY; <?= date("Y") ?>  </strong></p>
+                  <p class="copyright_text"><strong> Event Judging System &COPY; <?= date("Y") ?> </strong></p>
                </div>
             </div>
          </div>
       </div>
       <!-- copyright section end -->
+
       <!-- Javascript files-->
       <script src="js/jquery.min.js"></script>
       <script src="js/popper.min.js"></script>
@@ -329,8 +295,15 @@
       <!-- sidebar -->
       <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
       <script src="js/custom.js"></script>
+
       <script>
-// Disable right-click
+        // Mobile menu toggle
+        document.getElementById('mediaButton').addEventListener('click', function() {
+            document.getElementById('mainListDiv').classList.toggle('show_list');
+        });
+
+        // Security measures
+        // Disable right-click
         document.addEventListener('contextmenu', function (e) {
             e.preventDefault();
         });
@@ -346,20 +319,10 @@
             }
         };
 
-        // Disable developer tools
-        function disableDevTools() {
-            if (window.devtools.isOpen) {
-                window.location.href = "about:blank";
-            }
-        }
-
-        // Check for developer tools every 100ms
-        setInterval(disableDevTools, 100);
-
         // Disable selecting text
         document.onselectstart = function (e) {
             e.preventDefault();
         };
-</script>
+      </script>
    </body>
 </html>
