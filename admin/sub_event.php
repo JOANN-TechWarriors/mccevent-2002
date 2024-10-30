2bdbf8d6b67901aa4301aba1832b0dcc716a70cd
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-30 16:13:45 +0800

    commit

--- admin/sub_event.php
+++ admin/sub_event.php
@@ -206,6 +206,8 @@ if (isset($_POST['edit_se'])) {
 <html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
     <!-- Include other necessary styles and scripts -->
