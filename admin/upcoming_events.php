4daafb092eb92f44dbae3b70e8058c808362c609
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-30 12:59:25 +0800

    commit

--- admin/upcoming_events.php
+++ admin/upcoming_events.php
@@ -8,7 +8,6 @@
 <head>
 <link rel="shortcut icon" href="../images/logo copy.png"/>
 <title>Event Judging System</title>  
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="..//assets/fullcalendar/main.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
