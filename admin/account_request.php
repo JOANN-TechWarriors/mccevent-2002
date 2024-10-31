34e31bd09aaa7f188940a598c70dd1bce2e3150a
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-31 17:45:28 +0800

    commit

--- admin/account_request.php
+++ admin/account_request.php
@@ -562,7 +562,7 @@ if (isset($_POST['deleteStudent'])) {
 
 </div>
 
-<?php include('..//admin/footer.php') ?>
+ <center><?php include('..//admin/footer.php') ?></center>
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
