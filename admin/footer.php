d75b220a4bba78698078dd1002b58c967da2af24
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-10-31 17:59:57 +0800

    commit

--- admin/footer.php
+++ admin/footer.php
@@ -1,11 +1,13 @@
-<div class="main">
+<center>
+  <div class="main">
  <!-- Footer
     ================================================== -->
     <footer >
       <div class="container">
       
-       <font size="2" class="pull-left"> <center><strong> Event Judging  System &COPY; <?= date("Y") ?></center>  </strong></font> <br />
+       <font size="2" class="pull-left"> <strong> Event Judging  System &COPY; <?= date("Y") ?></strong></font> <br />
         
       </div>
     </footer>
 </div>
+</center>  
