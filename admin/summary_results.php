9fba0604c58b7a5d2abb118ac8f067e97a891668
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-11-11 12:07:48 +0800

    commit

--- admin/summary_results.php
+++ admin/summary_results.php
@@ -158,10 +158,6 @@ $active_main_event=$_GET['main_event_id'];
             margin: 0.5in;
             background: white; /* White background for content wrapper */
         }
-        .place-1st { color: gold; font-weight: bold; }
-        .place-2nd { color: #C0C0C0; font-weight: bold; }
-        .place-3rd { color: #CD7F32; font-weight: bold; }
-        .other-place { color: #000; }
     </style>
 </head>
  
@@ -185,79 +181,59 @@ $active_main_event=$_GET['main_event_id'];
                 </div>
                 <?php }  ?>
 
-                <div class="table-responsive">
-                    <table align="center">
-                        <?php   
-                        $sy_query = $conn->query("select DISTINCT sy FROM main_event where organizer_id='$session_id' AND mainevent_id='$active_main_event='") or die(mysql_error());
-                        while ($sy_row = $sy_query->fetch()) {
-                            $sy=$sy_row['sy'];
-                            $MEctrQuery = $conn->query("select * FROM main_event where sy='$sy'") or die(mysql_error());
-                            $MECtr = $MEctrQuery->rowCount();
-                        ?>
-                        <tr>
-                            <td>   
+                        <div class="table-responsive">
+                            <table align="center">
                                 <?php   
-                                $event_query = $conn->query("select * from main_event where organizer_id='$session_id' AND sy='$sy'") or die(mysql_error());
-                                while ($event_row = $event_query->fetch()) {
-                                    $main_event_id=$event_row['mainevent_id'];
-                                    $SEctrQuery = $conn->query("select * FROM sub_event where mainevent_id='$main_event_id'") or die(mysql_error());
-                                    while($SECtr = $SEctrQuery->fetch()) {
-                                        $rs_subevent_id=$SECtr['subevent_id'];
+                                $sy_query = $conn->query("select DISTINCT sy FROM main_event where organizer_id='$session_id' AND mainevent_id='$active_main_event='") or die(mysql_error());
+                                while ($sy_row = $sy_query->fetch()) {
+                                    $sy=$sy_row['sy'];
+                                    $MEctrQuery = $conn->query("select * FROM main_event where sy='$sy'") or die(mysql_error());
+                                    $MECtr = $MEctrQuery->rowCount();
                                 ?>
-                                <div class="sub-event">
-                                    <h4>EVENT: <strong><?php echo $SECtr['event_name']; ?></strong></h4>
-                                    <div class="table-responsive">
-                                        <table class="table table-bordered">
-                                            <tr>
-                                                <?php   
-                                                $contxx_query = $conn->query("select DISTINCT fullname from contestants where subevent_id='$rs_subevent_id'") or die(mysql_error());
-                                                while ($contxx_row = $contxx_query->fetch()) { 
-                                                ?>
-                                                <th><center><?php echo $contxx_row['fullname']; ?></center></th>
-                                                <?php } ?>
-                                            </tr>
-                                            <tr>
-                                                <?php  
-                                                $contxxz_query = $conn->query("select contestant_id from contestants where subevent_id='$rs_subevent_id'") or die(mysql_error());
-                                                while ($contxxz_row = $contxxz_query->fetch()) {  
-                                                    $contxzID=$contxxz_row['contestant_id'];
-                                                    // Modified query to order by total_score or relevant ranking field
-                                                    $place_query = $conn->query("
-                                                        SELECT 
-                                                            CASE 
-                                                                WHEN total_score = (SELECT MAX(total_score) FROM sub_results WHERE subevent_id='$rs_subevent_id') THEN '1st Place'
-                                                                WHEN total_score = (SELECT MAX(total_score) FROM sub_results WHERE subevent_id='$rs_subevent_id' AND total_score < (SELECT MAX(total_score) FROM sub_results WHERE subevent_id='$rs_subevent_id')) THEN '2nd Place'
-                                                                WHEN total_score = (SELECT MAX(total_score) FROM sub_results WHERE subevent_id='$rs_subevent_id' AND total_score < (SELECT MAX(total_score) FROM sub_results WHERE subevent_id='$rs_subevent_id' AND total_score < (SELECT MAX(total_score) FROM sub_results WHERE subevent_id='$rs_subevent_id'))) THEN '3rd Place'
-                                                                ELSE CONCAT(ROW_NUMBER() OVER (ORDER BY total_score DESC), 'th Place')
-                                                            END as place_title,
-                                                            total_score
-                                                        FROM sub_results 
-                                                        WHERE contestant_id='$contxzID' 
-                                                        AND subevent_id='$rs_subevent_id'
-                                                        ORDER BY total_score DESC
-                                                        LIMIT 1
-                                                    ") or die(mysql_error());
-                                                    
-                                                    while ($place_row = $place_query->fetch()) {
-                                                        $place = $place_row['place_title'];
-                                                        $class = '';
-                                                        if (strpos($place, '1st') !== false) $class = 'place-1st';
-                                                        else if (strpos($place, '2nd') !== false) $class = 'place-2nd';
-                                                        else if (strpos($place, '3rd') !== false) $class = 'place-3rd';
-                                                        else $class = 'other-place';
-                                                ?>
-                                                <td><center><span class="<?php echo $class; ?>"><?php echo $place; ?></span></center></td>
-                                                <?php } } ?>
-                                            </tr>
-                                        </table>
-                                    </div>
-                                </div>
-                                <?php } } ?>
-                            </td>
-                        </tr>
-                        <?php } ?>
-                    </table>
-                </div>
+                                <tr>
+                                    <td>   
+                                        <?php   
+                                        $event_query = $conn->query("select * from main_event where organizer_id='$session_id' AND sy='$sy'") or die(mysql_error());
+                                        while ($event_row = $event_query->fetch()) {
+                                            $main_event_id=$event_row['mainevent_id'];
+                                            $SEctrQuery = $conn->query("select * FROM sub_event where mainevent_id='$main_event_id'") or die(mysql_error());
+                                            while($SECtr = $SEctrQuery->fetch()) {
+                                                $rs_subevent_id=$SECtr['subevent_id'];
+                                        ?>
+                                        <div class="sub-event">
+                                            <h4>EVENT: <strong><?php echo $SECtr['event_name']; ?></strong></h4>
+                                            <div class="table-responsive">
+                                                <table class="table table-bordered">
+                                                    <tr>
+                                                    <?php   
+                                                    $contxx_query = $conn->query("select DISTINCT fullname from contestants where subevent_id='$rs_subevent_id'") or die(mysql_error());
+                                                    while ($contxx_row = $contxx_query->fetch()) { 
+                                                    ?>
+                                                        <th><center><?php echo $contxx_row['fullname']; ?></center></th>
+                                                    <?php } ?>
+                                                    </tr>
+                                                    <tr>
+                                                    <?php  
+                                                    $contxxz_query = $conn->query("select contestant_id from contestants where subevent_id='$rs_subevent_id'") or die(mysql_error());
+                                                    while ($contxxz_row = $contxxz_query->fetch()) {  
+                                                        $contxzID=$contxxz_row['contestant_id'];
+                                                        $place_query = $conn->query("select DISTINCT place_title from sub_results where contestant_id='$contxzID' AND subevent_id='$rs_subevent_id'") or die(mysql_error());
+                                                        while ($place_row = $place_query->fetch()) { 
+                                                    ?>
+                                                        <td><center><?php echo $place_row['place_title']; ?></center></td>
+                                                    <?php } } ?>
+                                                    </tr>
+                                                </table>
+                                            </div>
+                                        </div>
+                                        <?php } } ?>
+                                    </td>
+                                </tr>
+                                <?php } ?>
+                            </table>
+                        </div>
+                    </div>
+                </section>
             </div>
         </div>
     </div>
