aec9b92e59ef79ae6187c991c5af3ae365bb25b1
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-11-13 19:21:23 +0800

    commit

--- admin/verify_gmail.php
+++ admin/verify_gmail.php
@@ -3,7 +3,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 require 'dbcon.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
