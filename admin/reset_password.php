824aef44da1230f57cdf770b60f87ec1b6d25e66
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-11-14 01:13:05 +0800

    commit

--- admin/reset_password.php
+++ admin/reset_password.php
@@ -108,13 +108,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </style>
 </head>
 <body>
-    <!-- Navbar -->
-    <div class="navbar">
-        <div class="container">
-            <h1>Event Judging System</h1>
-        </div>
-    </div>
-
     <!-- Header -->
     <header class="jumbotron subhead" id="overview">
         <div class="container">
