d618e1ff72ab305b9d18c9a4e6febd6b969bead7
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-11-28 23:42:32 +0800

    commit

--- sql.php
+++ sql.php
@@ -13,37 +13,33 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// SQL to select all main events
-$sql = "SELECT * FROM main_event";
+// SQL to select all logs
+$sql = "SELECT * FROM logs";
 $result = $conn->query($sql);
 
 // Check if there are any results
 if ($result->num_rows > 0) {
     echo "<table border='1'>
             <tr>
-                <th>Main Event ID</th>
-                <th>Event Name</th>
-                <th>Status</th>
-                <th>Organizer ID</th>
-                <th>School Year</th>
-                <th>Date Start</th>
-                <th>Date End</th>
-                <th>Place</th>
-                <th>Banner</th>
+                <th>ID</th>
+                <th>IP</th>
+                <th>Username</th>
+                <th>Timestamp</th>
+                <th>Latitude</th>
+                <th>Longitude</th>
+                <th>Type</th>
             </tr>";
     
     // Output data of each row
     while($row = $result->fetch_assoc()) {
         echo "<tr>
-                <td>" . htmlspecialchars($row["mainevent_id"]) . "</td>
-                <td>" . htmlspecialchars($row["event_name"]) . "</td>
-                <td>" . htmlspecialchars($row["status"]) . "</td>
-                <td>" . htmlspecialchars($row["organizer_id"]) . "</td>
-                <td>" . htmlspecialchars($row["sy"]) . "</td>
-                <td>" . htmlspecialchars($row["date_start"]) . "</td>
-                <td>" . htmlspecialchars($row["date_end"]) . "</td>
-                <td>" . htmlspecialchars($row["place"]) . "</td>
-                <td>" . htmlspecialchars($row["banner"]) . "</td>
+                <td>" . htmlspecialchars($row["id"]) . "</td>
+                <td>" . htmlspecialchars($row["ip"]) . "</td>
+                <td>" . htmlspecialchars($row["username"]) . "</td>
+                <td>" . htmlspecialchars($row["timestamp"]) . "</td>
+                <td>" . htmlspecialchars($row["latitude"]) . "</td>
+                <td>" . htmlspecialchars($row["longitude"]) . "</td>
+                <td>" . htmlspecialchars($row["type"]) . "</td>
               </tr>";
     }
     echo "</table>";
