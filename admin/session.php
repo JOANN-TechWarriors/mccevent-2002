470e8a96291d020472b432e51836a498265edc72
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-11-28 14:09:12 +0800

    commit

--- admin/session.php
+++ admin/session.php
@@ -14,7 +14,7 @@ session_start();
 // Check whether the session variable SESS_MEMBER_ID is present or not
 if (!isset($_SESSION['id']) || ($_SESSION['id'] == '')) { ?>
     <script>
-        window.location = 'index.php';
+        window.location = 'index2';
     </script>
 <?php
     exit();
