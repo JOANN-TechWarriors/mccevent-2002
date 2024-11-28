1e17aba3e498f031b26697cd04c9e7117fdc9959
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-11-28 15:08:43 +0800

    commit

--- tabulator/dashboard.php
+++ tabulator/dashboard.php
@@ -337,7 +337,7 @@ if (substr($request, -4) == '.php') {
             cancelButtonText: 'No'
         }).then((result) => {
             if (result.isConfirmed) {
-                window.location.href = '..//index.php';
+                window.location.href = '..//index';
             }
         });
     });
