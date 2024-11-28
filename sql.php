b42d9d3325f5bfc510e38aae7ffbf794ebba2bf0
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-11-29 00:37:13 +0800

    commit

--- sql.php
+++ sql.php
@@ -5,38 +5,20 @@ $username = 'u510162695_judging_root';
 $password = '1Judging_root';
 $dbname = 'u510162695_judging';
 
-// Create connection
-$conn = new mysqli($host, $username, $password, $dbname);
+try {
+    // Create a new PDO connection
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    // Reset auto-increment for main_event
+    $reset_auto_increment_sql = "ALTER TABLE main_event AUTO_INCREMENT = 1";
+    $conn->exec($reset_auto_increment_sql);
+    echo "Auto-increment value reset for main_event.<br>";
 
-// SQL to alter the mainevent_id column in main_event to BIGINT
-$alter_main_event_id_sql = "ALTER TABLE main_event MODIFY COLUMN mainevent_id BIGINT(20) NOT NULL AUTO_INCREMENT";
-if ($conn->query($alter_main_event_id_sql) === TRUE) {
-    echo "Table main_event altered successfully for mainevent_id.<br>";
-} else {
-    echo "Error altering main_event table for mainevent_id: " . $conn->error . "<br>";
-}
-
-// SQL to alter the subevent_id column in sub_event to BIGINT
-$alter_sub_event_id_sql = "ALTER TABLE sub_event MODIFY COLUMN subevent_id BIGINT(20) NOT NULL AUTO_INCREMENT";
-if ($conn->query($alter_sub_event_id_sql) === TRUE) {
-    echo "Table sub_event altered successfully for subevent_id.<br>";
-} else {
-    echo "Error altering sub_event table for subevent_id: " . $conn->error . "<br>";
-}
-
-// SQL to alter the mainevent_id column in sub_event to BIGINT
-$alter_sub_event_main_event_id_sql = "ALTER TABLE sub_event MODIFY COLUMN mainevent_id BIGINT(20) NOT NULL";
-if ($conn->query($alter_sub_event_main_event_id_sql) === TRUE) {
-    echo "Table sub_event altered successfully for mainevent_id.<br>";
-} else {
-    echo "Error altering sub_event table for mainevent_id: " . $conn->error . "<br>";
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
 }
 
 // Close connection
-$conn->close();
+$conn = null;
 ?>
