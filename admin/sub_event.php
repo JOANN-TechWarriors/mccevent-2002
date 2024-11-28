9d5afaab0b201eb418879d52869d47a6d9de86c9
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-11-29 00:22:11 +0800

    commit

--- admin/sub_event.php
+++ admin/sub_event.php
@@ -41,13 +41,6 @@ function generateUniqueId() {
     return uniqid(rand(), true);
 }
 
-// Your existing code
-$request = $_SERVER['REQUEST_URI'];
-if (substr($request, -4) == '.php') {
-    $new_url = substr($request, 0, -4);
-    header("Location: $new_url", true, 301);
-    exit();
-}
 // Handle form submission
 if (isset($_POST['add_event'])) {
     $banner = $_FILES['banner']['name'];
