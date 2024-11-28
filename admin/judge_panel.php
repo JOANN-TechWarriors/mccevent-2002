7a6f4753f4c4c94fda9a63692b6fab3afed53be2
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-11-28 13:09:51 +0800

    commit

--- admin/judge_panel.php
+++ admin/judge_panel.php
@@ -82,7 +82,7 @@
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="caret"></span></a>
                   <ul class="dropdown-menu" role="menu">
               <li>
-                <a href="..//index.php">Logout</a>
+                <a href="..//index">Logout</a>
               </li>
               
       
