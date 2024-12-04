e18c08d9a69aeb0086abfd2280f0c7117b996b1a
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-12-04 17:56:26 +0800

    commit

--- admin/header.php
+++ admin/header.php
@@ -1,34 +1,4 @@
-<head>
-    <meta charset="utf-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content=""/>
-    <meta name="author" content=""/>
-    <link rel="shortcut icon" href="../img/logo.png"/>
-    <title>Event Judging System</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="..//includes/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
-      <link href="..//assets/css/docs.css" rel="stylesheet"/>
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="../assets/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif] -->
-
-    <!-- Custom styles for this template -->
-    <link href="carousel.css" rel="stylesheet"/>
-    
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.12/dist/sweetalert2.all.min.js"></script>
-    
-    <script type="text/javascript">
+<script type="text/javascript">
     // Disable right-click with an alert
     document.addEventListener('contextmenu', function(event) {
         event.preventDefault();
@@ -113,7 +83,3 @@ if (preg_match("/(" . implode("|", $disallowedUserAgents) . ")/i", $_SERVER['HTT
     exit("Unauthorized access");
 }
 ?>
-
-  </head>
-
-  
