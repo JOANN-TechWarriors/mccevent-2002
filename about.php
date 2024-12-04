8f423368f476156671ab2eb3e989212fe3f4413a
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-12-04 22:49:31 +0800

    commit

--- about.php
+++ about.php
@@ -437,7 +437,7 @@ if (substr($request, -4) == '.php') {
             <div class="row">
                 <div  class="col-md-8 mx-auto text-center">
                     <h2 class="about_title mb-3">Welcome to MCC Events</h2>
-                    <p style="text align: justify" class="about_text mb-5">
+                    <p style="text-align: justify" class="about_text mb-5">
                         The Madridejos Community College (MCC) Events is your partner in transforming school events. 
                         Our platform streamlines event management with automated judging, live streaming, and audience interaction, 
                         fostering transparency, engagement, and community connection.
