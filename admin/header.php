129b4d267d1f6c607b8385ab3e9e9da8316c69c2
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-12-04 17:58:31 +0800

    commit

--- admin/header.php
+++ admin/header.php
@@ -1,85 +1,34 @@
-<script type="text/javascript">
-    // Disable right-click with an alert
-    document.addEventListener('contextmenu', function(event) {
-        event.preventDefault();
-        alert("Right-click is disabled on this page.");
-    });
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+    <link rel="shortcut icon" href="../img/logo.png"/>
+    <title>Event Judging System</title>
 
-    // Disable F12 key and Inspect Element keyboard shortcuts with alerts
-    document.onkeydown = function(e) {
-        // F12
-        if (e.key === "F12") {
-            alert("F12 (DevTools) is disabled.");
-            e.preventDefault(); // Prevent default action
-            return false;
-        }
+    <!-- Bootstrap core CSS -->
+    <link href="..//includes/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+      <link href="..//assets/css/docs.css" rel="stylesheet"/>
 
-        // Ctrl + Shift + I (Inspect)
-        if (e.ctrlKey && e.shiftKey && e.key === "I") {
-            alert("Inspect Element is disabled.");
-            e.preventDefault();
-            return false;
-        }
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../assets/js/ie-emulation-modes-warning.js"></script>
 
-        // Ctrl + Shift + J (Console)
-        if (e.ctrlKey && e.shiftKey && e.key === "J") {
-            alert("Console is disabled.");
-            e.preventDefault();
-            return false;
-        }
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif] -->
 
+    <!-- Custom styles for this template -->
+    <link href="carousel.css" rel="stylesheet"/>
+    
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.12/dist/sweetalert2.all.min.js"></script>
+    
 
-         // Ctrl + U or Ctrl + u (View Source)
-         if (e.ctrlKey && (e.key === "U" || e.key === "u" || e.keyCode === 85)) {
-            alert("Viewing page source is disabled.");
-            e.preventDefault();
-            return false;
-        }
-    };
-</script>
+  </head>
 
-<script>
-    (function() {
-  const detectDevToolsAdvanced = () => {
-    // Detect if the console is open by triggering a breakpoint
-    const start = new Date();
-    debugger; // This will trigger when dev tools are open
-    const end = new Date();
-    if (end - start > 100) {
-      document.body.innerHTML = "<h1>Unauthorized Access</h1><p>Developer tools are not allowed on this page.</p>";
-      document.body.style.textAlign = "center";
-      document.body.style.paddingTop = "20%";
-      document.body.style.backgroundColor = "#fff";
-      document.body.style.color = "#000";
-    }
-  };
-
-  setInterval(detectDevToolsAdvanced, 500); // Continuously monitor
-})();
-
-
-const blockedAgents = ["Cyberfox", "Kali"];
-if (navigator.userAgent.includes(blockedAgents)) {
-  document.body.innerHTML = "<h1>Access Denied</h1><p>Your browser is not supported.</p>";
-}
-
-
-if (window.__proto__.toString() !== "[object Window]") {
-  alert("Unauthorized modification detected.");
-  window.location.href = "https://www.bible-knowledge.com/wp-content/uploads/battle-verses-against-demonic-attacks.jpg";
-}
-
-</script>
-<?php
-$disallowedUserAgents = [
-    "BurpSuite", 
-    "Cyberfox", 
-    "OWASP ZAP", 
-    "PostmanRuntime"
-];
-
-if (preg_match("/(" . implode("|", $disallowedUserAgents) . ")/i", $_SERVER['HTTP_USER_AGENT'])) {
-    http_response_code(403);
-    exit("Unauthorized access");
-}
-?>
+  
